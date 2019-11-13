0d3282dfb8d4b5d8224f0dcf988e9e373a866261
QIBO <34388425+fathur-rahman@users.noreply.github.com>
2019-11-13 17:48:58 +0700

    LOGIN Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Login | SC Inovasi</title>
+        <title>LOGIN | SC Inovasi</title>
         <meta name="description" content="">
         <meta name="keywords" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">

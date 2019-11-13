ab65682252f5991ff4a8c1324b9d2351a80116b6
caturbgs <caturbgs@github.com>
2019-11-13 19:23:00 +0700

    Update create.blade.php

--- resources/views/pilars/create.blade.php
+++ resources/views/pilars/create.blade.php
@@ -19,7 +19,7 @@
                         <nav class="breadcrumb-container" aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item">
-                                    <a href="/dashboard"><i class="ik ik-home"></i></a>
+                                    <a href="/pilars"><i class="ik ik-home"></i></a>
                                 </li>
                                 <li class="breadcrumb-item active"  aria-current="page">Kelola</li>
                             </ol>

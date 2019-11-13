28e6e39d548d3271bfc47bbce2f7b094a27af69b
QIBO <34388425+fathur-rahman@users.noreply.github.com>
2019-11-13 17:45:26 +0700

    tab logo di login diganti

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,8 +8,8 @@
         <meta name="keywords" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon" />
+        <link rel="icon" href="{{asset('assets/src/img/icon.png')}}" type="image/x-icon" />
+        <!-- <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon" /> -->
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
         
